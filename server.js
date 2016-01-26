698052dce786cb454bd62082e8834370a0474dbe
Bobby <singh.b@husky.neu.edu>
2016-01-26 18:03:04 -0500

    Updated server.js to use separate function for hello url

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
-app.get('/hello', function(req, res) {
-	  res.send('hello world');
-});
+app.get('/hello', sayHello);
+
+function sayHello(req, res) {
+    res.send('Hello world!');
+}
 
 app.listen(port, ipaddress);
