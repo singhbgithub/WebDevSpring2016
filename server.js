849741eaaa17e9f6b5b1cf6d1b756f2dc368f064
Bobby <singh.b@husky.neu.edu>
2016-01-26 19:01:55 -0500

    Reorganized server.js ordering of lines.

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
-app.get('/hello', sayHello);
-
 function sayHello(req, res) {
     res.send('Hello world!');
 }
 
+app.use(express.static(__dirname + '/public'));
+app.get('/hello', sayHello);
 app.listen(port, ipaddress);
