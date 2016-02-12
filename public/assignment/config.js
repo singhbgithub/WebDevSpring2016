cffa5721ead9a49c582d871d83e9c2e285eee44c
Bobby <singh.b@husky.neu.edu>
2016-02-11 23:49:12 -0500

    Added Angular app and config.

---
+++ public/assignment/config.js
@@ -0,0 +1,17 @@
+(function(){
+    angular.config(function($routeProvider){
+        $routeProvider
+            .when('/', {
+                templateUrl: 'home/home.view.html'
+            })
+            .when('/profile', {
+                templateUrl: 'users/profile.view.html'
+            })
+            .when('/admin', {
+                templateUrl: 'users/admin.view.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
+})();
