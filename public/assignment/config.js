a4647fa84a7531858206afa2ae27b4307073eeec
Bobby <singh.b@husky.neu.edu>
2016-02-13 22:11:43 -0500

    Fix urls and drop cruft from old views

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -2,13 +2,34 @@
     angular.module('FormBuilderApp').config(function($routeProvider){
         $routeProvider
             .when('/', {
-                templateUrl: 'home/home.view.html'
-            })
-            .when('/profile', {
-                templateUrl: 'users/profile.view.html'
+                templateUrl: 'views/home/home.view.html'
             })
             .when('/admin', {
-                templateUrl: 'users/admin.view.html'
+                templateUrl: 'views/admin/admin.view.html'
+            })
+            .when('/forms', {
+                templateUrl: 'views/forms/forms.view.html'
+            })
+            .when('/fields', {
+                templateUrl: 'views/forms/form-fields.view.html'
+            })
+            .when('/header', {
+                templateUrl: 'views/header/header.view.html'
+            })
+            .when('/home', {
+                templateUrl: 'views/home/home.view.html'
+            })
+            .when('/sidebar', {
+                templateUrl: 'views/sidebar/sidebar.view.html'
+            })
+            .when('/profile', {
+                templateUrl: 'views/users/profile.view.html'
+            })
+            .when('/login', {
+                templateUrl: 'views/users/login.view.html'
+            })
+            .when('/register', {
+                templateUrl: 'views/users/register.view.html'
             })
             .otherwise({
                 redirectTo: '/'
