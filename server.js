a9dd89b5a320a848bae5498e6131d12c7b355a97
Bobby <singh.b@husky.neu.edu>
2016-03-12 12:19:27 -0500

    Clean up server style

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 (function() {
     'use strict';
-    var express = require('express');
-    var app = express();
-    var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-    var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-
-    function sayHello(req, res) {
-        res.send('Hello world!');
-    }
+    var express = require('express'),
+        app = express(),
+        ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+        port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
     app.use(express.static(__dirname + '/public'));
-    app.get('/hello', sayHello);
     app.listen(port, ipaddress);
 })();
