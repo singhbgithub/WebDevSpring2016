c35768793fe321bc06fa62d3de2bc4d4b555e7fd
singhbgithub <bobby.singh@compass.com>
2016-04-06 15:34:35 -0400

    Move main event handler declaration to the top

--- public/assignment/client/views/main/main.controller.js
+++ public/assignment/client/views/main/main.controller.js
@@ -4,12 +4,17 @@
 
     function MainController($location, $rootScope) {
         var mainVm = this;
+
+        // Event Handlers
+        $rootScope.$on('userLoggedIn', userLoggedIn);
+        $rootScope.$on('userLoggedOut', userLoggedOut);
+
+        // Scope Variables
         mainVm.$location = $location;
-        // Get around the ng-include not updating b/c it does not keep references to prim values;
-        // wrap in object.
+        // Get around the ng-include not updating b/c it does not keep references to prim values; wrap in object.
         $rootScope.user = {'loggedIn': false, 'isAdmin': false};
-        // Listen for a user login event.
-        $rootScope.$on('userLoggedIn', function(event, args) {
+
+        function userLoggedIn(event, args) {
             // Read from our user service - this does not include current state
             // i.e. login & admin status
             if (args && args.hasOwnProperty('user')) {
@@ -29,13 +34,13 @@
             }
             console.log('Logged In. User: ', $rootScope.user);
             mainVm.$location = $location.path('/profile');
-        });
-        // Listen for a user logout event.
-        $rootScope.$on('userLoggedOut', function() {
+        }
+
+        function userLoggedOut() {
             console.log('user logged out');
             // make separate var to hold transient state TODO(bobby)
             $rootScope.user = {'loggedIn': false, 'isAdmin': false};
             mainVm.$location = $location.path('/');
-        });
+        }
     }
 })();
