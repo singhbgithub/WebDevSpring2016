e695756f25920a029f4553ffddf4ced622080c83
singhbgithub <bobby.singh@compass.com>
2016-04-06 18:03:35 -0400

    app.js accepts mongoose db API and connection

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -1,11 +1,13 @@
 (function() {
     'use strict';
-    module.exports = function (app) {
-        // TODO(bobby): why are we passing in the models?
-        var userModel = require(__dirname + '/models/user.model.js')();
+    module.exports = function (app, mongoose) {
+        // Load user model & service.
+        var userModel = require(__dirname + '/models/user.model.js')(mongoose);
         require(__dirname + '/services/user.service.js')(app, userModel);
-        var formModel = require(__dirname + '/models/form.model.js')();
+        // Load form model & service.
+        var formModel = require(__dirname + '/models/form.model.js')(mongoose);
         require(__dirname + '/services/form.service.js')(app, formModel);
+        // Load field model & service.
         require(__dirname + '/services/field.service.js')(app, formModel);
     };
 })();
--- server.js
+++ server.js
@@ -2,14 +2,16 @@
     'use strict';
     var express = require('express'),
         bodyParser = require('body-parser'),
+        mongoose = require('mongoose'),
         multer = require('multer'),
         assignmentServerModule = require(__dirname + '/public/assignment/server/app.js'),
         app = express(),
         ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
-        port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+        port = process.env.OPENSHIFT_NODEJS_PORT || 3000,
+        connectionString = 'mongodb://127.0.0.1:27017/webdev2016';
 
+    // Root of the site.
     app.use(express.static(__dirname + '/public'));
-
     // For parsing application/json
     app.use(bodyParser.json());
     // For parsing application/x-www-form-urlencoded
@@ -17,8 +19,20 @@
     // For parsing multipart/form-data
     app.use(multer());
 
-    // Load the server component for the assignment's directory.
-    assignmentServerModule(app);
-
-    app.listen(port, ipaddress);
+    // Connect to the database dependent on environment.
+    if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+    mongoose.connect(connectionString);
+    var db = mongoose.connection;
+     db.on('error', console.error.bind(console, 'connection error:'));
+     db.once('open', function() {
+         // Load the server for the assignment directory.
+         assignmentServerModule(app, mongoose);
+         app.listen(port, ipaddress);
+     });
 })();
