d365c4eb7494a6e46548457be49ce5b27dc02862
singhbgithub <bobby.singh@compass.com>
2016-04-06 18:20:02 -0400

    Added field schema

---
+++ public/assignment/server/models/field.schema.js
@@ -0,0 +1,18 @@
+(function() {
+    'use strict';
+    
+    module.exports = function(mongoose) {
+        var fieldSchema = mongoose.Schema({
+            'label': String,
+            'type': {
+                'type': String,
+                'enum': ['TEXT', 'TEXTAREA', 'EMAIL', 'PASSWORD', 'OPTIONS', 'DATE', 'RADIOS', 'CHECKBOXES'],
+                'default': 'TEXT',
+                'required': true
+            },
+            'placeholder': String,
+            'options': [{'label': String, 'value': String}]
+        }, {'collection': 'field'});
+        return fieldSchema;
+    };
+})();
