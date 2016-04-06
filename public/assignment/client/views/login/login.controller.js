db16e8340b6932a529cf77bb995c66d7f8d3d812
singhbgithub <bobby.singh@compass.com>
2016-04-06 15:31:34 -0400

    Move login event handler declaration to the top

--- public/assignment/client/views/login/login.controller.js
+++ public/assignment/client/views/login/login.controller.js
@@ -4,27 +4,30 @@
 
     function LoginController($location, $rootScope, UserService) {
         var loginVm = this;
+
+        // Event Handlers
+        loginVm.login = login;
+
         // Must not be logged-in to view this page.
-        if (!$rootScope.user.loggedIn) {
-            loginVm.login = function (username, password) {
-                if (username && password) {
-                    UserService.findUserByCredentials(username, password)
-                        .then(function(user) {
-                            if (user) {
-                                $rootScope.$broadcast('userLoggedIn', {'user': user});
-                            } else {
-                                window.alert('No associated account. Please sign up.');
-                                loginVm.$location = $location.path('/register');
-                            }
-                        });
-                }
-                else {
-                    window.alert('Enter your information.');
-                }
-            };
-        }
-        else {
+        if ($rootScope.user.loggedIn) {
             loginVm.$location = $location.path('/profile');
+        }
+
+        function login(username, password) {
+            if (username && password) {
+                UserService.findUserByCredentials(username, password)
+                    .then(function(user) {
+                        if (user) {
+                            $rootScope.$broadcast('userLoggedIn', {'user': user});
+                        } else {
+                            window.alert('No associated account. Please sign up.');
+                            loginVm.$location = $location.path('/register');
+                        }
+                    });
+            }
+            else {
+                window.alert('Enter your information.');
+            }
         }
     }
 })();
