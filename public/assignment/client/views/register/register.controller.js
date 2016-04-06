ad52daa806c73b0a9e409ce42d013d3d369ff4b2
singhbgithub <bobby.singh@compass.com>
2016-04-06 15:37:19 -0400

    Move register event handler declaration to the top

--- public/assignment/client/views/register/register.controller.js
+++ public/assignment/client/views/register/register.controller.js
@@ -4,25 +4,28 @@
 
     function RegisterController($location, $rootScope, UserService) {
         var registerVm = this;
+
+        // Event Handlers
+        registerVm.create = create;
+
         // Must not be logged-in to view this page.
-        if (!$rootScope.user.loggedIn) {
-            registerVm.create = function (user) {
-                if (user) {
-                    UserService.createUser(user)
-                        .then(function(response) {
-                            var responseUser = response[response.length + - 1];
-                            console.log('Account Created. User:', responseUser);
-                            $rootScope.$broadcast('userLoggedIn', {'user': responseUser});
-                        });
-                    registerVm.$location = $location.path('/profile');
-                }
-                else {
-                    window.alert('Enter your information.');
-                }
-            };
-        }
-        else {
+        if ($rootScope.user.loggedIn) {
             registerVm.$location = $location.path('/profile');
+        }
+
+        function create(user) {
+            if (user) {
+                UserService.createUser(user)
+                    .then(function(response) {
+                        var responseUser = response[response.length + - 1];
+                        console.log('Account Created. User:', responseUser);
+                        $rootScope.$broadcast('userLoggedIn', {'user': responseUser});
+                    });
+                registerVm.$location = $location.path('/profile');
+            }
+            else {
+                window.alert('Enter your information.');
+            }
         }
     }
 })();
