6c765c877d2313f4176cd8667120d835af8e2ad6
singhbgithub <bobby.singh@compass.com>
2016-04-06 15:25:58 -0400

    Move form event handler declaration to the top

--- public/assignment/client/views/forms/forms.controller.js
+++ public/assignment/client/views/forms/forms.controller.js
@@ -4,63 +4,69 @@
 
     function FormController($location, $rootScope, FormService) {
         var formVm = this;
+
+        // Event Handlers
+        formVm.addForm = addForm;
+        formVm.updateForm = updateForm;
+        formVm.deleteForm = deleteForm;
+        formVm.selectForm = selectForm;
+
         // We have a logged-in user.
         if ($rootScope.user.loggedIn) {
-
-            // Populate the forms
-            var findFormsForUserAndSetScope = function() {
-                FormService.findAllFormsForUserId($rootScope.user._id)
-                    .then(function(forms) {
-                        formVm.forms = forms;
-                    });
-            };
             findFormsForUserAndSetScope();
-
-            // Handlers
-            formVm.addForm = function (formTitle) {
-                var form = {'title': formTitle},
-                    userId = $rootScope.user._id;
-                FormService.createFormForUser(userId, form)
-                    .then(function() {
-                        findFormsForUserAndSetScope();
-                        // Clear the current form and input text.
-                        formVm.form.title = '';
-                        formVm.currentForm = undefined;
-                    });
-            };
-
-            formVm.updateForm = function (formTitle) {
-                if (formVm.currentForm !== null && formVm.currentForm !== undefined) {
-                    var form = {'title': formTitle},
-                        formId = formVm.currentForm._id;
-                    FormService.updateFormById(formId, form)
-                        .then(function() {
-                            findFormsForUserAndSetScope();
-                        });
-                }
-            };
-
-            formVm.deleteForm = function (index) {
-                var formId = formVm.forms[index]._id;
-                FormService.deleteFormById(formId)
-                    .then(function() {
-                        findFormsForUserAndSetScope();
-                        formVm.currentForm = undefined;
-                    });
-            };
-
-            formVm.selectForm = function (index) {
-                formVm.currentForm = formVm.forms[index];
-                // In case the form obj has not been instantiated.
-                if (formVm.form == undefined || formVm.form == null) {
-                    formVm.form = {};
-                }
-                formVm.form.title = formVm.currentForm.title;
-            };
-
         }
         else {
             formVm.$location = $location.path('/register');
+        }
+
+        // Populate the forms
+        function findFormsForUserAndSetScope () {
+            FormService.findAllFormsForUserId($rootScope.user._id)
+                .then(function(forms) {
+                    formVm.forms = forms;
+                });
+        }
+
+        // Handlers
+        function addForm(formTitle) {
+            var form = {'title': formTitle},
+                userId = $rootScope.user._id;
+            FormService.createFormForUser(userId, form)
+                .then(function() {
+                    findFormsForUserAndSetScope();
+                    // Clear the current form and input text.
+                    formVm.form.title = '';
+                    formVm.currentForm = undefined;
+                });
+        }
+
+        function updateForm(formTitle) {
+            if (formVm.currentForm !== null && formVm.currentForm !== undefined) {
+                var form = {'title': formTitle},
+                    formId = formVm.currentForm._id;
+                FormService.updateFormById(formId, form)
+                    .then(function() {
+                        findFormsForUserAndSetScope();
+                    });
+            }
+        }
+
+        function deleteForm(index) {
+            var formId = formVm.forms[index]._id;
+            FormService.deleteFormById(formId)
+                .then(function() {
+                    findFormsForUserAndSetScope();
+                    formVm.currentForm = undefined;
+                });
+        }
+
+        function selectForm(index) {
+            formVm.currentForm = formVm.forms[index];
+            // In case the form obj has not been instantiated.
+            if (formVm.form == undefined || formVm.form == null) {
+                formVm.form = {};
+            }
+            formVm.form.title = formVm.currentForm.title;
         }
     }
 })();
