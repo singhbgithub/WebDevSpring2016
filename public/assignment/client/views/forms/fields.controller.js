6c71a7530da234c8d850a039cd3e73101dbd7f86
singhbgithub <bobby.singh@compass.com>
2016-04-06 15:20:54 -0400

    Move field event handler declaration to the top

--- public/assignment/client/views/forms/fields.controller.js
+++ public/assignment/client/views/forms/fields.controller.js
@@ -5,91 +5,109 @@
     function FieldController($location, $rootScope, $routeParams, $uibModal, FormService,
                              FieldService) {
         var fieldVm = this;
+
+        // Event Handlers
+        fieldVm.editField = editField;
+        fieldVm.cloneField = cloneField;
+        fieldVm.removeField = removeField;
+        fieldVm.addField = addField;
+
+        // Scope Variables
+        fieldVm.sortableOptions = sortableOptionsGenerator();
+
         // We have a logged-in user.
         if ($rootScope.user.loggedIn) {
             fieldVm.formId = $routeParams.formId;
-
-            // Populate the fields
-            var findFieldsForFormAndSetScope = function() {
-                FieldService.getFieldsForForm(fieldVm.formId)
-                    .then(function (response) {
-                            fieldVm.fields = response;
-                        });
-            };
             findFieldsForFormAndSetScope();
+        } else {
+            fieldVm.$location = $location.path('/register');
+        }
 
-            fieldVm.editField = function(field) {
-                var modalInstance = $uibModal.open({
-                    animation: true,
-                    templateUrl: 'fieldModal.html',
-                    controller: 'ModalInstanceController',
-                    resolve: {
-                        field: function () {
-                            return field;
-                        }
+        // Populate the fields
+        function findFieldsForFormAndSetScope() {
+            FieldService.getFieldsForForm(fieldVm.formId)
+                .then(function (response) {
+                    fieldVm.fields = response;
+                });
+        }
+
+        function editField(field) {
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'fieldModal.html',
+                controller: 'ModalInstanceController',
+                resolve: {
+                    field: function () {
+                        return field;
                     }
-                });
-                modalInstance.result.then(function (field) {
-                    FieldService.updateField(fieldVm.formId, field._id, field)
-                        .then(function() {
-                            findFieldsForFormAndSetScope();
-                        });
-                });
-            };
-
-            fieldVm.cloneField = function(field) {
-                FieldService.createFieldForForm(fieldVm.formId, field)
+                }
+            });
+            modalInstance.result.then(function (field) {
+                FieldService.updateField(fieldVm.formId, field._id, field)
                     .then(function() {
                         findFieldsForFormAndSetScope();
                     });
-            };
+            });
+        }
 
-            fieldVm.removeField = function(field) {
-                FieldService.deleteFieldFromForm(fieldVm.formId, field._id)
-                    .then(function() {
-                        findFieldsForFormAndSetScope();
-                    });
-            };
+        function cloneField(field) {
+            FieldService.createFieldForForm(fieldVm.formId, field)
+                .then(function() {
+                    findFieldsForFormAndSetScope();
+                });
+        }
 
-            /* Used for drag and drop ordering. */
-            fieldVm.sortableOptions = {
+        function removeField(field) {
+            FieldService.deleteFieldFromForm(fieldVm.formId, field._id)
+                .then(function() {
+                    findFieldsForFormAndSetScope();
+                });
+        }
+
+        /* Used for drag and drop ordering. Made to be a function to allow for declaration
+        * of the VM events at the top w/out linting errors. */
+        function sortableOptionsGenerator() {
+            return {
                 handle: '.sortableHandle',
-                update: function(e, ui) {
+                update: function (e, ui) {
                     FormService.updateFormById(fieldVm.formId, {'fields': fieldVm.fields})
-                        .then(function(form) {
+                        .then(function (form) {
                             console.log('Form Updated', form);
                             findFieldsForFormAndSetScope();
                         });
                 }
-            };
+            }
+        }
 
-            fieldVm.addField = function(newFieldType) {
-                FieldService.createFieldForForm(fieldVm.formId, {'type': newFieldType})
-                    .then(function() {
-                        findFieldsForFormAndSetScope();
+        function addField(newFieldType) {
+            FieldService.createFieldForForm(fieldVm.formId, {'type': newFieldType})
+                .then(function() {
+                    findFieldsForFormAndSetScope();
                 });
-            };
-        } else {
-            fieldVm.$location = $location.path('/register');
         }
     }
 
     angular.module('FormBuilderApp').controller('ModalInstanceController', ModalInstanceController);
     function ModalInstanceController($scope, $uibModalInstance, field) {
-        // TODO(bobby): vm style is funky with uiModal... figure out later
+        // TODO(bobby): vm style is funky with nested controllers
         var modalVm = $scope;
-        modalVm.fieldToEdit = angular.copy(field);
-        modalVm.hasPlaceholder = field.type === 'TEXT' || field.type === 'TEXTAREA' ||
-            field.type === 'EMAIL';
-        modalVm.hasOptions = field.type === 'OPTIONS' || field.type === 'CHECKBOXES' ||
-            field.type === 'RADIOS';
 
+        // Event Handlers
+        modalVm.update = update;
+        modalVm.cancel = cancel;
+        
+        // Scope Variables
+        modalVm.fieldToEdit = angular.copy(field);
+        modalVm.hasPlaceholder = field.type === 'TEXT' || field.type === 'TEXTAREA' || field.type === 'EMAIL';
+        modalVm.hasOptions = field.type === 'OPTIONS' || field.type === 'CHECKBOXES' || field.type === 'RADIOS';
         modalVm.fieldToEdit.optionsText = '';
+
+        // Add the options as text
         angular.forEach(modalVm.fieldToEdit.options || [], function(option) {
-           modalVm.fieldToEdit.optionsText += option.label + ';' + option.value + '\n';
+            modalVm.fieldToEdit.optionsText += option.label + ';' + option.value + '\n';
         });
 
-        modalVm.update = function () {
+        function update() {
             var optionsTextFragments = modalVm.fieldToEdit.optionsText.split('\n'),
                 options = [];
             angular.forEach(optionsTextFragments, function(optionTextFragment) {
@@ -100,10 +118,10 @@
             });
             modalVm.fieldToEdit.options = options;
             $uibModalInstance.close(modalVm.fieldToEdit);
-        };
+        }
 
-        modalVm.cancel = function () {
+        function cancel() {
             $uibModalInstance.dismiss('cancel');
-        };
+        }
     }
 })();
