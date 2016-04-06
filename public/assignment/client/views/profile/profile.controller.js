1b99e423dd71b16cfd26213e056a61f9341f7167
singhbgithub <bobby.singh@compass.com>
2016-04-06 15:35:44 -0400

    Move profile event handler declaration to the top

--- public/assignment/client/views/profile/profile.controller.js
+++ public/assignment/client/views/profile/profile.controller.js
@@ -4,26 +4,29 @@
 
     function ProfileController($location, $rootScope, UserService) {
         var profileVm = this;
+
+        // Event Handlers
+        profileVm.update = update;
+
         // Must be logged in to view this page.
-        if ($rootScope.user.loggedIn) {
-            profileVm.update = function (updatedUser) {
-                if (updatedUser) {
-                    UserService.updateUser($rootScope.user._id, updatedUser)
-                        .then(function(response) {
-                            // FIXME(bobby): transient state fields again
-                            response.loggedIn = $rootScope.user.loggedIn;
-                            response.isAdmin = $rootScope.user.isAdmin;
-                            $rootScope.user = response;
-                            console.log('Account Updated. User: ', response);
-                    });
-                }
-                else {  // This code may not be hittable TODO(bobby)
-                    window.alert('Enter your information.');
-                }
-            };
-        }
-        else {
+        if (!$rootScope.user.loggedIn) {
             profileVm.$location = $location.path('/register');
+        }
+
+        function update(updatedUser) {
+            if (updatedUser) {
+                UserService.updateUser($rootScope.user._id, updatedUser)
+                    .then(function(response) {
+                        // FIXME(bobby): transient state fields again
+                        response.loggedIn = $rootScope.user.loggedIn;
+                        response.isAdmin = $rootScope.user.isAdmin;
+                        $rootScope.user = response;
+                        console.log('Account Updated. User: ', response);
+                    });
+            }
+            else {  // This code may not be hittable TODO(bobby)
+                window.alert('Enter your information.');
+            }
         }
     }
 })();
