029ce162c4753d24aad17fce01e6690aeb1e1c9f
singhbgithub <bobby.singh@compass.com>
2016-04-07 20:12:04 -0400

    Remove unused server and client util files

--- public/assignment/client/js/util.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-    // Array Remove - By John Resig (MIT Licensed)
-    Array.remove = function(arr, from, to) {
-        var rest = arr.slice((to || from) + 1 || arr.length);
-        arr.length = from < 0 ? arr.length + from : from;
-        return arr.push.apply(arr, rest);
-    };
-})();
