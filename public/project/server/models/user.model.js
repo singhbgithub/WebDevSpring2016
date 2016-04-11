89612acc5b5fd62f14656b1c4c86c54f3e8bb6da
singhbgithub <bobby.singh@compass.com>
2016-04-11 09:55:42 -0400

    Project user model uses schema; delete mock data

--- public/project/server/models/user.model.js
+++ public/project/server/models/user.model.js
@@ -1,13 +1,12 @@
 (function() {
     'use strict';
 
-    var q = require('q'), /* Dependencies */
-        userMock = require('./user.mock.json');
+    var q = require('q'); /* Dependencies */
 
     /* Add a node module */
-    module.exports = function() {
-
-        var model = {
+    module.exports = function(mongoose, userSchema) {
+        var User = mongoose.model('User-Project', userSchema),
+        model = {
             'createUser': createUser,
             'findUserById': findUserById,
             'findUserByUsername': findUserByUsername,
@@ -19,168 +18,106 @@
 
         return model;
 
-        function createUser(user) {
+        function createUser(createUserRequest) {
             var deferred = q.defer();
-
-            setTimeout(function () {
-                findUserByUsername(user.username)
-                    .then(function (user) {
-                        deferred.reject('Account already exists for :' + user.username);
-                    }, function () {
-                        user._id = new Date().getTime();
-                        userMock.push(user);
-                        deferred.resolve(user);
-                    });
-            }, 100);
-
+            User.create(createUserRequest, function (err, user) {
+                if (err) {
+                    deferred.reject(err);
+                } else if (!user) {
+                    deferred.reject('User creation experienced unknown failure!');
+                } else {
+                    deferred.resolve(user);
+                }
+            });
             return deferred.promise;
         }
 
         function findAllUsers() {
             var deferred = q.defer();
-
-            setTimeout(function () {
-                deferred.resolve(userMock);
-            }, 100);
-
+            User.find({}, function(err, users) {
+                if(err) {
+                    deferred.reject(err);
+                } else {
+                    deferred.resolve(users);
+                }
+            });
             return deferred.promise;
         }
 
         function findUserById(id) {
             var deferred = q.defer();
-
-            setTimeout(function () {
-                var user = null,
-                    userId = typeof id === 'string' ? parseInt(id) : id;
-                for (var i = 0; i < userMock.length; i++) {
-                    var userI = userMock[i];
-                    if (userI._id === userId) {
-                        user = userI;
-                        break;
-                    }
-                }
-                if (user) {
-                    deferred.resolve(user);
+            User.findById(id, function(err, user) {
+                if(err) {
+                    deferred.reject(err);
+                } else if (!user) {
+                    deferred.reject('No user with id ' + id + ' found.');
                 } else {
-                    deferred.reject(user);
+                    deferred.resolve(user);
                 }
-            }, 100);
-
+            });
             return deferred.promise;
         }
 
         function findUserByUsername(username) {
             var deferred = q.defer();
-
-            setTimeout(function () {
-                var user = null;
-                for (var i = 0; i < userMock.length; i++) {
-                    var userI = userMock[i];
-                    if (userI.username === username) {
-                        user = userI;
-                        break;
-                    }
-                }
-                if (user) {
-                    deferred.resolve(user);
+            User.findOne({'username': username}, function(err, user) {
+                if(err) {
+                    deferred.reject(err);
+                } else if (!user) {
+                    deferred.reject('User ' + username + ' not found.');
                 } else {
-                    deferred.reject(user);
+                    deferred.resolve(user);
                 }
-            }, 100);
-
+            });
             return deferred.promise;
         }
 
         function findUserByCredentials(username, password) {
             var deferred = q.defer();
-
-            setTimeout(function () {
-                var user = null;
-                for (var i = 0; i < userMock.length; i++) {
-                    var userI = userMock[i];
-                    if (userI.username === username && userI.password === password) {
-                        user = userI;
-                        break;
-                    }
-                }
-                if (user) {
+            User.findOne({'username': username, 'password': password}, function(err, user) {
+                if(err) {
+                    deferred.reject(err);
+                } else if (!user) {
+                    deferred.reject('User ' + username + ' not found with specified credentials.');
+                } else {
                     deferred.resolve(user);
-                } else {
-                    deferred.reject(user);
                 }
-            }, 100);
-
+            });
             return deferred.promise;
         }
 
-        function updateUserById(userId, updateUserRequest) {
+        function updateUserById(id, updateUserByIdRequest) {
             var deferred = q.defer();
-
-            setTimeout(function () {
-                var updatedUser = null,
-                    id = typeof userId === 'string' ? parseInt(userId) : userId;
-                for (var i = 0; i < userMock.length; i++) {
-                    var currentUser = userMock[i];
-                    if (currentUser._id === id) {
-                        // FIXME(bobby): not updating all the fields properly b/c this logic will go away w/ mongo
-                        if (updateUserRequest.hasOwnProperty('emails')) {
-                            currentUser.emails = updateUserRequest.emails;
-                        }
-                        if (updateUserRequest.hasOwnProperty('phones')) {
-                            currentUser.phones = updateUserRequest.phones;
-                        }
-                        if (updateUserRequest.hasOwnProperty('firstName')) {
-                            currentUser.firstName = updateUserRequest.firstName;
-                        }
-                        if (updateUserRequest.hasOwnProperty('lastName')) {
-                            currentUser.lastName = updateUserRequest.lastName;
-                        }
-                        if (updateUserRequest.hasOwnProperty('password')) {
-                            currentUser.password = updateUserRequest.password;
-                        }
-                        updatedUser = currentUser;
-                        break;
-                    }
-                }
-                if (updatedUser) {
-                    deferred.resolve(updatedUser);
+            User.findByIdAndUpdate(id, updateUserByIdRequest, function(err) {
+                if(err) {
+                    deferred.reject(err);
                 } else {
-                    deferred.reject('Could not find user with id: ' + userId);
+                    User.findById(id, function (err, user) {
+                        if(err) {
+                            deferred.reject(err);
+                        } else if (!user) {
+                            deferred.reject('No updated user with id ' + id + ' found.');
+                        } else {
+                            deferred.resolve(user);
+                        }
+                    });
                 }
-            }, 100);
-
+            });
             return deferred.promise;
         }
 
-        function deleteUserById(userId) {
+        function deleteUserById(id) {
             var deferred = q.defer();
-
-            setTimeout(function () {
-                var deletedUser = null,
-                    id = typeof userId === 'string' ? parseInt(userId) : userId;
-                for (var i = 0; i < userMock.length; i++) {
-                    var currentUser = userMock[i];
-                    if (currentUser._id === id) {
-                        _remove(userMock, i);
-                        deletedUser = currentUser;
-                        break;
-                    }
-                }
-                if (deletedUser) {
-                    deferred.resolve(deletedUser);
+            User.findByIdAndRemove(id, function(err, user) {
+                if(err) {
+                    deferred.reject(err);
+                } else if (!user) {
+                    deferred.reject('No user with id ' + id + ' found for deletion.');
                 } else {
-                    deferred.reject('Could not find user with id: ' + userId);
+                    deferred.resolve(user);
                 }
-            }, 100);
-
+            });
             return deferred.promise;
-        }
-
-        // Array Remove - By John Resig (MIT Licensed) - put in reusable module TODO(bobby)
-        function _remove(arr, from, to) {
-            var rest = arr.slice((to || from) + 1 || arr.length);
-            arr.length = from < 0 ? arr.length + from : from;
-            return arr.push.apply(arr, rest);
         }
     }
 })();
