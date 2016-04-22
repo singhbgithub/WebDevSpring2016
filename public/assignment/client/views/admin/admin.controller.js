3ebd9094e284eb4f36344046723f7b360cb3e318
singhbgithub <bobby.singh@compass.com>
2016-04-22 11:32:52 -0400

    Update admin controller and view to use service.
    Also admin view can sort ascending/descending by fields.

--- public/assignment/client/views/admin/admin.controller.js
+++ public/assignment/client/views/admin/admin.controller.js
@@ -1,8 +1,86 @@
 (function() {
     'use strict';
+
     angular.module('FormBuilderApp').controller('AdminController', AdminController);
 
-    function AdminController($scope, $location, $rootScope) {
-        // Nothing yet...
+    function AdminController(AdminService) {
+        var adminVm = this;
+
+        // Event Handlers
+        adminVm.addUser = addUser;
+        adminVm.updateUser = updateUser;
+        adminVm.deleteUser = deleteUser;
+        adminVm.selectUser = selectUser;
+        // Scope Variables
+        adminVm.error = '';
+        adminVm.users = [];
+        adminVm.newUser = {};
+        adminVm.selectedUser = undefined;
+        adminVm.reverseSort = false;
+        adminVm.orderByField = 'username';
+
+        findAllUsersAndSetScope();
+
+        function findAllUsersAndSetScope() {
+            AdminService.findAllUsers()
+                .then(function(users) {
+                    users.forEach(function (user) {
+                        user.roles = user.roles.join();
+                    });
+                    adminVm.users = users;
+                    adminVm.error = '';
+                });
+        }
+
+        function addUser(newUser) {
+            var user = angular.copy(newUser);
+            user.roles = user && user.roles ? user.roles.split(',') : [];
+            AdminService.createUser(user)
+                .then(function () {
+                    adminVm.newUser = {};
+                    adminVm.selectedUser = undefined;
+                    findAllUsersAndSetScope();
+                }, function (err) {
+                    adminVm.error = err;
+                });
+        }
+
+        function updateUser(updatedUser) {
+            if (adminVm.selectedUser !== null && adminVm.selectedUser !== undefined) {
+                var user = angular.copy(updatedUser),
+                    userId = adminVm.selectedUser._id;
+                user.roles = user && user.roles ? user.roles.split(',') : [];
+                AdminService.updateUser(userId, user)
+                    .then(function () {
+                        adminVm.newUser = {};
+                        adminVm.selectedUser = undefined;
+                        findAllUsersAndSetScope();
+                    }, function (err) {
+                        adminVm.error = err;
+                    });
+            }
+        }
+
+        function deleteUser(index) {
+            var userId = adminVm.users[index]._id;
+            AdminService.deleteUserById(userId)
+                .then(function () {
+                    findAllUsersAndSetScope();
+                    adminVm.selectedUser = undefined;
+                }, function (err) {
+                    adminVm.error = err;
+                });
+        }
+        
+        function selectUser(index) {
+            adminVm.selectedUser = adminVm.users[index];
+            adminVm.newUser = {
+                username: adminVm.selectedUser.username,
+                password: adminVm.selectedUser.password,
+                firstName: adminVm.selectedUser.firstName,
+                lastName: adminVm.selectedUser.lastName,
+                roles: adminVm.selectedUser.roles
+            };
+        }
     }
 })();
