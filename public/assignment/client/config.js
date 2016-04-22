ffd159cd4ea031f8e9d66e422164502ca4111638
singhbgithub <bobby.singh@compass.com>
2016-04-22 05:36:36 -0400

    Route config resolves if user is logged in

--- public/assignment/client/config.js
+++ public/assignment/client/config.js
@@ -1,6 +1,6 @@
 (function() {
     'use strict';
-    angular.module('FormBuilderApp').config(function($routeProvider){
+    angular.module('FormBuilderApp').config(function($routeProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'views/home/home.view.html'
@@ -11,12 +11,14 @@
             .when('/forms', {
                 templateUrl: 'views/forms/forms.view.html',
                 controller: 'FormController',
-                controllerAs: 'formVm'
+                controllerAs: 'formVm',
+                resolve: {loggedIn: isLoggedIn}
             })
             .when('/form/:formId/fields', {
                 templateUrl: 'views/forms/fields.view.html',
                 controller: 'FieldController',
-                controllerAs: 'fieldVm'
+                controllerAs: 'fieldVm',
+                resolve: {loggedIn: isLoggedIn}
             })
             .when('/home', {
                 templateUrl: 'views/home/home.view.html'
@@ -24,20 +26,63 @@
             .when('/profile', {
                 templateUrl: 'views/profile/profile.view.html',
                 controller: 'ProfileController',
-                controllerAs: 'profileVm'
+                controllerAs: 'profileVm',
+                resolve: {loggedIn: isLoggedIn}
             })
             .when('/login', {
                 templateUrl: 'views/login/login.view.html',
                 controller: 'LoginController',
-                controllerAs: 'loginVm'
+                controllerAs: 'loginVm',
+                resolve: {isNotLoggedIn: isNotLoggedIn}
             })
             .when('/register', {
                 templateUrl: 'views/register/register.view.html',
                 controller: 'RegisterController',
-                controllerAs: 'registerVm'
+                controllerAs: 'registerVm',
+                resolve: {isNotLoggedIn: isNotLoggedIn}
             })
             .otherwise({
                 redirectTo: '/'
             });
     });
+
+    function isLoggedIn($q, $http, $location, $rootScope) {
+        var deferred = $q.defer();
+
+        $http.get('/api/assignment/loggedin').then(function(response) {
+            if (response.data && response.data !== '0') {
+                $rootScope.user = {
+                    'loggedIn': true,
+                    'isAdmin': false, // TODO(bobby): add logic here when roles added to schema
+                    'obj': response.data
+                };
+                deferred.resolve();
+            } else {
+                $location.url('/login');
+                deferred.reject();
+            }
+        }, function() {
+            $location.url('/login');
+            deferred.reject();
+        });
+
+        return deferred.promise;
+    }
+
+    function isNotLoggedIn($q, $http, $location) {
+        var deferred = $q.defer();
+
+        $http.get('/api/assignment/loggedin').then(function(response) {
+            if (response.data && response.data === '0') {
+                deferred.resolve();
+            } else {
+                $location.url('/profile');
+                deferred.reject();
+            }
+        }, function() {
+            deferred.reject();
+        });
+
+        return deferred.promise;
+    }
 })();
