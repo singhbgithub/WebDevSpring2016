bb687c3e312ff19cf458bfd1e7777a9f489662c8
singhbgithub <bobby.singh@compass.com>
2016-04-22 08:54:17 -0400

    *Route config resolves if user is logged in

--- public/project/client/config.js
+++ public/project/client/config.js
@@ -8,17 +8,20 @@
             .when('/login', {
                 templateUrl: 'views/login/login.view.html',
                 controller: 'LoginController',
-                controllerAs: 'loginVm'
+                controllerAs: 'loginVm',
+                resolve: {isNotLoggedIn: isNotLoggedIn}
             })
             .when('/register', {
                 templateUrl: 'views/register/register.view.html',
                 controller: 'RegisterController',
-                controllerAs: 'registerVm'
+                controllerAs: 'registerVm',
+                resolve: {isNotLoggedIn: isNotLoggedIn}
             })
             .when('/profile', {
                 templateUrl: 'views/profile/profile.view.html',
                 controller: 'ProfileController',
-                controllerAs: 'profileVm'
+                controllerAs: 'profileVm',
+                resolve: {loggedIn: isLoggedIn}
             })
             .when('/search', {
                 templateUrl: 'views/search/search.view.html',
@@ -34,4 +37,43 @@
                 redirectTo: '/'
             });
     });
+
+    function isLoggedIn($q, $http, $location, $rootScope) {
+        var deferred = $q.defer();
+
+        $http.get('/api/project/loggedin').then(function(response) {
+            if (response.data && response.data !== '0') {
+                $rootScope.user = {
+                    'loggedIn': true,
+                    'obj': response.data
+                };
+                deferred.resolve();
+            } else {
+                $location.url('/login');
+                deferred.reject();
+            }
+        }, function() {
+            $location.url('/login');
+            deferred.reject();
+        });
+
+        return deferred.promise;
+    }
+
+    function isNotLoggedIn($q, $http, $location) {
+        var deferred = $q.defer();
+
+        $http.get('/api/project/loggedin').then(function(response) {
+            if (response.data && response.data === '0') {
+                deferred.resolve();
+            } else {
+                $location.url('/profile');
+                deferred.reject();
+            }
+        }, function() {
+            deferred.reject();
+        });
+
+        return deferred.promise;
+    }
 })();
