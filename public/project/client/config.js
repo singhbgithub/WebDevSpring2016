fa8551f2c47bcc63efc2aaa34bf41a1d37ae9213
singhbgithub <bobby.singh@compass.com>
2016-04-27 05:04:29 -0400

    Add login state to all routes

--- public/project/client/config.js
+++ public/project/client/config.js
@@ -5,7 +5,8 @@
     function config($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: 'views/home/home.view.html'
+                templateUrl: 'views/home/home.view.html',
+                resolve: {isNotLoggedIn: isNotLoggedIn}
             })
             .when('/login', {
                 templateUrl: 'views/login/login.view.html',
@@ -28,16 +29,37 @@
             .when('/search', {
                 templateUrl: 'views/search/search.view.html',
                 controller: 'SearchController',
-                controllerAs: 'searchVm'
+                controllerAs: 'searchVm',
+                resolve: {setLoggedInUser: setLoggedInUser}
             })
             .when('/content', {
                 templateUrl: 'views/content/content.view.html',
                 controller: 'ContentController',
-                controllerAs: 'contentVm'
+                controllerAs: 'contentVm',
+                resolve: {setLoggedInUser: setLoggedInUser}
             })
             .otherwise({
                 redirectTo: '/'
             });
+    }
+
+    function setLoggedInUser($q, $http, $rootScope) {
+        var deferred = $q.defer();
+
+        $http.get('/api/assignment/loggedin').then(function(response) {
+            var user = response.data;
+            if (user && user !== '0') {
+                $rootScope.user = {
+                    'loggedIn': true,
+                    'obj': user
+                };
+            }
+            deferred.resolve();
+        }, function() {
+            deferred.resolve();
+        });
+
+        return deferred.promise;
     }
 
     function isLoggedIn($q, $http, $location, $rootScope) {
