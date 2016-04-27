fc0b0d392b202d086206c309b4831498cee6c344
singhbgithub <bobby.singh@compass.com>
2016-04-27 18:58:57 -0400

    Enable tag web service

--- public/project/server/app.js
+++ public/project/server/app.js
@@ -2,12 +2,15 @@
     'use strict';
     module.exports = function (app, mongoose, userModel) {
         var reviewSchema = require(__dirname + '/models/review.schema.js')(mongoose);
+        var tagSchema = require(__dirname + '/models/tag.schema.js')(mongoose);
         var contentSchema = require(__dirname + '/models/content.schema.js')(mongoose);
         var reviewModel = require(__dirname + '/models/review.model.js')(mongoose, reviewSchema);
+        var tagModel = require(__dirname + '/models/tag.model.js')(mongoose, tagSchema);
         var contentModel = require(__dirname + '/models/content.model.js')(mongoose, contentSchema);
         require(__dirname + '/services/user.service.js')(app, userModel);
         require(__dirname + '/services/security.service.js')(app, userModel);
         require(__dirname + '/services/review.service.js')(app, reviewModel);
+        require(__dirname + '/services/tag.service.js')(app, tagModel);
         require(__dirname + '/services/content.service.js')(app, contentModel);
     };
 })();
