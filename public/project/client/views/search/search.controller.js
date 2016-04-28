7a693fc88d1205e7a9c8c10a6f31ab7f10c77069
singhbgithub <bobby.singh@compass.com>
2016-04-28 09:18:08 -0400

    Search query in url to remember state

--- public/project/client/views/search/search.controller.js
+++ public/project/client/views/search/search.controller.js
@@ -11,35 +11,48 @@
         searchVm.results = [];
         searchVm.hasNoResults = false;
 
+        prepareSearch();
+
         function search(searchQuery) {
             console.log('Search Query: ', searchQuery);
+            searchVm.$location = $location.search({'q': searchQuery});
             if (searchQuery) {
-                // TODO(bobby): paginate
-                // TODO(bobby): multiple tags
-                var tag = searchQuery;
-                ContentService.findContentByTag(tag)
-                    .then(function (contentList) {
-                        var hasResults = contentList && contentList.length;
-                        searchVm.results = hasResults ? contentList: [];
-                        searchVm.hasNoResults = !hasResults;
-                        console.log('Found content: ', contentList);
-                    }, function (err) {
-                        searchVm.error = 'Could not search for that tag. An error occurred.';
-                        console.log(err);
-
-                    });
                 // TODO(bobby): load content on the page. And slide the search bar up.
                 // TODO(bobby): Add typeahead for search box
+                // TODO(bobby): paginate
+                // TODO(bobby): multiple tags
+                searchByTag(searchQuery);
             }
             else {
                 window.alert('Enter a search.');
             }
         }
 
+        function searchByTag(tag) {
+            ContentService.findContentByTag(tag)
+                .then(function (contentList) {
+                    var hasResults = contentList && contentList.length;
+                    searchVm.results = hasResults ? contentList: [];
+                    searchVm.hasNoResults = !hasResults;
+                    console.log('Found content: ', contentList);
+                }, function (err) {
+                    searchVm.error = 'Could not search for that tag. An error occurred.';
+                    console.log(err);
+                });
+        }
+
         function selectContent(index) { // TODO(bobby): move to rootScope function.
             $rootScope.currentContent = searchVm.results[index];
             console.log('Current Content:', $rootScope.currentContent);
+            $location.search('q', null); // Clear the query.
             searchVm.$location = $location.path('/content');
+        }
+
+        function prepareSearch() {
+            var searchParams = $location.search();
+            if (searchParams.q) {
+                searchByTag(searchParams.q);
+            }
         }
     }
 })();
